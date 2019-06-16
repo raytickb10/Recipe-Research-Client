adca0902cd342de17a4507a1529f385a162479e9
raytickb10 <raytickb10@students.ecu.edu>
2019-06-16 05:10:29 -0400

    Update header-bar.js

--- src/components/header-bar.js
+++ src/components/header-bar.js
@@ -14,7 +14,7 @@ export class HeaderBar extends React.Component {
         let logOutButton;
         if (this.props.loggedIn) {
             logOutButton = (
-                <button class="form__button" onClick={() => this.logOut()}>Log out</button>
+                <button class="form__button" onClick={() => this.logOut()}>LOG OUT</button>
             );
         }
         return (
