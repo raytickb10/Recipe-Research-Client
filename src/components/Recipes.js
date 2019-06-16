50952b9a8c43b133efa9aad8f16f963803f9b02a
raytickb10 <raytickb10@students.ecu.edu>
2019-06-15 21:54:23 -0400

    Update Recipes.js

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -7,7 +7,7 @@ const Recipes = props => (
     <div className="row">
     { props.recipes && props.recipes.map((recipe) => {
       return (
-        <div key={recipe.title} className="col-md-4" style={{ marginBottom:"2rem" }}>
+        <div key={recipe.title} className="col-md-4" >
           <div className="recipes__box">
             <img 
               className="recipe__box-img" 
