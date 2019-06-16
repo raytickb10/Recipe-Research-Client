fe329d151a7a8ef048c79b638879fa569bd1b3f2
raytickb10 <raytickb10@students.ecu.edu>
2019-06-15 21:54:43 -0400

    Update Router.js

--- src/components/Router.js
+++ src/components/Router.js
@@ -3,12 +3,16 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import App from "../App";
 import Recipe from "./Recipe";
+import RegistrationPage from "./registration-page";
+import LandingPage from "./landing-page";
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" component={App} exact />
       <Route path="/recipe/:id" component={Recipe} />
+      <Route path="/login" component={LandingPage} />
+      <Route path="/register" component={RegistrationPage} />
     </Switch>
   </BrowserRouter>
 );
