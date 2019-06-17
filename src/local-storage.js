93bbd397c23455dc57bb2a91ad204a82b23e42b7
raytickb10 <raytickb10@students.ecu.edu>
2019-06-16 20:12:26 -0400

    Update local-storage.js

--- src/local-storage.js
+++ src/local-storage.js
@@ -4,7 +4,6 @@ export const loadAuthToken = () => {
 
 export const saveAuthToken = authToken => {
     console.log(localStorage.getItem('authToken'));
-    console.log(localStorage.getItem('username'));
     try {
         localStorage.setItem('authToken', authToken);
         document.getElementById("login_text").disabled = true;
@@ -12,9 +11,17 @@ export const saveAuthToken = authToken => {
     } catch (e) {}
 };
 
+export const saveUsername = userName => {
+    console.log(localStorage.getItem('userName'));
+    try{
+        localStorage.setItem('userName', userName);
+    } catch (e) {}
+};
+
 export const clearAuthToken = () => {
     try {
         localStorage.removeItem('authToken');
+        localStorage.removeItem('userName');
         document.getElementById("login_text").disabled = false;
         document.getElementById("login_text").style.visibility = "visible";
     } catch (e) {}
