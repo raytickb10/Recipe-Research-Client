d9064787ee0ca94ca3a037d9c3eeb213214299cd
raytickb10 <raytickb10@students.ecu.edu>
2019-06-16 20:04:21 -0400

    Update local-storage.js

--- src/local-storage.js
+++ src/local-storage.js
@@ -1,10 +1,10 @@
 export const loadAuthToken = () => {
-    console.log(localStorage.getItem('authToken'));
-    console.log(localStorage.getItem('username'));
     return localStorage.getItem('authToken');
 };
 
 export const saveAuthToken = authToken => {
+    console.log(localStorage.getItem('authToken'));
+    console.log(localStorage.getItem('username'));
     try {
         localStorage.setItem('authToken', authToken);
         document.getElementById("login_text").disabled = true;
