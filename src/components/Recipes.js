15c278d51905f72e7134490e4104f843e400a7b5
raytickb10 <raytickb10@students.ecu.edu>
2019-06-18 04:53:45 -0400

    Update Recipes.js

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Link } from "react-router-dom";
 
 const Recipes = props => (
-  const [value] = React.useState(
+  const value = React.useState(
     if(localStorage.getItem('authToken') !== null){
       'hidden'
     }
