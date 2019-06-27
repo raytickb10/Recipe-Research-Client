30b4234af4a72d4025f183a28ebf2b2b3b3c0e81
raytickb10 <raytickb10@students.ecu.edu>
2019-06-27 06:18:52 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ class App extends Component {
           </div>
         </header>
         <Form getRecipe={this.getRecipe} />
-        <h2 className="explain_text" style={{visibility: this.state.explanation}}>explain, Then get . the goose</h2>
+        <h2 className="explain_text" style={{visibility: this.state.explanation}}>Welcome to Recipe Researcher, a quick and easy guide to all things food. Get started by first logging in with our test account (username:tester password: password123) or sign up for a free account today. Then thousands of recipes are only a click away. So way are you waiting for, get started now!</h2>
         <div style={{visibility: this.state.showandtell}}>
           <Recipes recipes={this.state.recipes} />
         </div>
