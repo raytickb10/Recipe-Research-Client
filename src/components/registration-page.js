8b96dd76355171d950d7b268e9ffb37bf17a5395
raytickb10 <raytickb10@students.ecu.edu>
2019-06-29 15:14:54 -0400

    Update registration-page.js

--- src/components/registration-page.js
+++ src/components/registration-page.js
@@ -14,7 +14,7 @@ export function RegistrationPage(props) {
         <div className="home">
         <div className="back-header"></div>
             <header className="App-header">    
-            <h2>Register for Recipe Researcher</h2>
+            <h2>Register for <br/> Recipe Researcher</h2>
             </header>
             <RegistrationForm />
             <div className="container signin">
