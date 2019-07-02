6709d70f609c2500415e792679a116130d41839b
raytickb10 <raytickb10@students.ecu.edu>
2019-07-01 21:14:09 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -36,16 +36,16 @@ class App extends Component {
     super();
     let value = '';
     let showoff = 'hidden';
-    let noshow = 'visible';
+    let noshow = 'block';
     if(localStorage.getItem('userName') !== null){
       value = 'Welcome, ' + localStorage.getItem('userName');
       showoff = 'visible';
-      noshow = 'hidden';
+      noshow = 'none';
     }
     else{
       value = '';
       showoff = 'hidden';
-      noshow = 'visible';
+      noshow = 'block';
     }
     this.state = {
       myText: value,
@@ -67,7 +67,7 @@ class App extends Component {
           </div>
         </header>
         <Form getRecipe={this.getRecipe} />
-        <h2 className="explain_text" style={{visibility: this.state.explanation}}>Welcome to Recipe Researcher, a quick and easy guide to all things food. Get started by first logging in with our test account (username:tester password: password123) or sign up for a free account today. Then thousands of recipes are only a click away. So what are you waiting for, get started now!</h2>
+        <h3 className="explain_text" style={{display: this.state.explanation}}>Welcome to Recipe Researcher, a quick and easy guide to all things food. Get started by first logging in with our test account (username:tester password: password123) or sign up for a free account today. Then thousands of recipes are only a click away. So what are you waiting for, get started now!</h2>
         <div style={{visibility: this.state.showandtell}}>
           <Recipes recipes={this.state.recipes} />
         </div>
